6cb60cf99ce3227341eea51096b8b8ba714aa853
Jefferson Arruda <153444125+JeffersonArruda98@users.noreply.github.com>
2024-02-19 21:05:22 -0300

    Add files via upload

---
+++ ex 43.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite um número:\n'))
+num2 = int(input('Digite outro número:\n'))
+if num1 > num2:
+    print('O primeiro valor é o maior.')
+elif num2 > num1:
+    print('O segundo valor é o maior.')
+else:
+    print('Os dois números são iguais.')
